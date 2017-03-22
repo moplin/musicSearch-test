ecb28c93ce72fb0e44b341ed9a524888d3e37312
moplin <pp@moplin.com>
2017-03-21 20:47:19 -0500

    testing codeship

---
+++ src/app/music-search/index/index.controller.spec.js
@@ -0,0 +1,20 @@
+(function() {
+    'use strict';
+
+    describe('controllers', function() {
+
+        beforeEach(module('app'));
+
+        it('hw should be helloWorld', inject(function($controller) {
+            var vm = $controller('ProfileController');
+
+            expect(vm.hw === 'helloWorld').toBeTruthy();
+
+        }));
+
+        it('Scope should be isolate', inject(function($controller) {
+            var vm = $controller('IndexController');
+            expect(vm).toEqual(jasmine.any(Object));
+        }));
+    });
+})();
