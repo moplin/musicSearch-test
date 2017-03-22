3ed41e7ebf47449f154ee3675333951cd2c1852c
moplin <pp@moplin.com>
2017-03-21 19:58:23 -0500

    first fixes don to index and routing

---
+++ src/app/layouts/body/body.controller.js
@@ -0,0 +1,32 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name BodyController
+ * @module body
+ * @kind function
+ *
+ * @description none yet
+ *
+ * Handles some stuff
+ */
+(function() {
+    'use strict';
+
+    angular
+        .module('app.musicsearch')
+        .controller('BodyStateController', BodyStateController);
+
+    /* @ngInject */
+    function BodyStateController($log) {
+        $log.log('BodyStateController');
+        //var loadingQueue = [];
+        //var vm = this;
+
+        //////////////// Functions
+
+
+        // watches
+
+    }
+})();
