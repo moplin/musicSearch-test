1b9ebbfba4843c4dba7232f2efe8f3afca6b0b1d
moplin <pp@moplin.com>
2017-03-22 16:13:24 -0500

    fixing procfile for heroku

--- e2e/main.spec.js
+++ e2e/main.spec.js
@@ -4,18 +4,12 @@ describe('The main view', function () {
   var page;
 
   beforeEach(function () {
-    browser.get('http://localhost:3000/index.html');
+    browser.get('http://localhost:5000/#/');
     page = require('./main.po');
   });
 
-  it('should include jumbotron with correct data', function() {
-    expect(page.h1El.getText()).toBe('\'Allo, \'Allo!');
-    expect(page.imgEl.getAttribute('src')).toMatch(/assets\/images\/yeoman.png$/);
-    expect(page.imgEl.getAttribute('alt')).toBe('I\'m Yeoman');
-  });
-
-  it('list more than 5 awesome things', function () {
-    expect(page.thumbnailEls.count()).toBeGreaterThan(5);
+  it('should include h1 with title', function() {
+    expect(page.h1El.getText()).toBe('helloWorld');
   });
 
 });
